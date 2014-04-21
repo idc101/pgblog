455175266503489f79cde9c3456a5a2a546e2150
Iain Cardnell <iain@cardnell.co.uk>
2014-04-21 21:09:04 +0800

    changed connection to mongohq

--- app.rb
+++ app.rb
@@ -5,10 +5,20 @@ require 'erb'
 require 'mongo'
 require 'date'
 require 'maruku'
+require 'uri'
 
-include Mongo
+def get_connection
+  return @db_connection if @db_connection
+  db = URI.parse(ENV['MONGOHQ_URL'])
+  db_name = db.path.gsub(/^\//, '')
+  @db_connection = Mongo::Connection.new(db.host, db.port).db(db_name)
+  @db_connection.authenticate(db.user, db.password) unless (db.user.nil? || db.user.nil?)
+  @db_connection
+end
 
-client = MongoClient.new # defaults to localhost:27017
+db = get_connection
+
+#client = MongoClient.new # defaults to localhost:27017
 db = client['pgblog-db']
 coll = db['posts']
 

2dd055914934bbe8f0c61b3cd3161ee773311d59
Iain Cardnell <iain@cardnell.co.uk>
2014-05-11 20:28:10 +0800

    Fixed bug in new post not submitting hash to mongo.

--- models/post.rb
+++ models/post.rb
@@ -4,17 +4,29 @@ class Post
   attr_accessor :id, :title, :url, :date, :tags, :text
 end
 
-def post_data_to_post(post_data)
+def hash_to_post(hash)
   post = Post.new
-  post.title= post_data['title']
-  post.url= post_data['url']
-  post.date= post_data['date']
-  post.text= post_data['text']
-  post.tags= post_data['tags']
+  post.title= hash['title']
+  post.url= hash['url']
+  post.date= hash['date']
+  post.text= hash['text']
+  post.tags= hash['tags']
   post
 end
 
+def post_to_hash(post)
+  hash = {
+  'title' => post.title,
+  'url' => post.url,
+  'date' => post.date,
+  'text' => post.text,
+  'tags' => post.tags
+  }
+  hash
+end
+
 def params_to_post(params)
+  post = Post.new
   post.title = params[:title]
   post.url = params[:url]
   post.text = params[:text]
@@ -24,20 +36,20 @@ def params_to_post(params)
 end
 
 def save_post(db, post)
-  db['posts'].insert post
+  db['posts'].insert post_to_hash(post)
 end
 
 def get_by_url(db, url)
   post_data = db['posts'].find_one("url" => url)
-  post_data_to_post post_data
+  hash_to_post post_data
 end
 
 def get_by_tag(db, tag)
-  posts = db['posts'].find("tags" => tag).map { |pd| post_data_to_post pd }
+  posts = db['posts'].find("tags" => tag).map { |pd| hash_to_post pd }
   posts
 end
 
 def get_recent_posts(db, start)
-  posts = db['posts'].find().sort( { :date => -1 } ).skip(start).limit(4).map { |pd| post_data_to_post pd }
+  posts = db['posts'].find().sort( { :date => -1 } ).skip(start).limit(4).map { |pd| hash_to_post pd }
   posts
 end
